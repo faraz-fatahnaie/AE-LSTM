d0a4da81833a121f7f2cd2d6319fcf38a4523cf0
Faraz <faraz.fatahnaie@gmail.com>
2023-12-26 01:31:20 +0330

    saving result / load previous hp / model initialization

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import csv
 import gc
 from itertools import product
 
-import keras.layers
 import pandas as pd
 import numpy as np
 from hyperopt import STATUS_OK, hp, Trials, fmin, tpe
@@ -18,15 +17,14 @@ from sklearn.model_selection import train_test_split
 from tensorflow.python.keras.layers import Dropout
 
 from utils import parse_data, OptimizerFactory
-from keras.models import Sequential, Model
-from keras.layers import Input, Dense, LSTM
-from keras.utils import to_categorical
-from keras.optimizers import Adam
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Input, Dense, LSTM
+from tensorflow.keras.optimizers import Adam
 from utils import set_seed
 
 # Set GPU device and disable eager execution
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
-physical_devices = tf.config.list_physical_devices('GPU')
+# os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+# physical_devices = tf.config.list_physical_devices('GPU')
 
 config = dict()
 
@@ -87,7 +85,7 @@ def train_ae(params, dataset_name):
     X_val = np.array(XValGlobal)
 
     if os.path.isfile(ae_path):
-        ae = keras.models.load_model(ae_path)
+        ae = tf.keras.models.load_model(ae_path)
         print(f'AE loaded from {ae_path}')
         tr_loss = ae.evaluate(X_train, X_train)
         val_loss = ae.evaluate(X_val, X_val)
@@ -95,8 +93,15 @@ def train_ae(params, dataset_name):
 
     else:
         ae = Sequential()
-        ae.add(LSTM(hidden_size, input_shape=(1, X_train.shape[2]), return_sequences=True))
-        ae.add(LSTM(X_train.shape[2], return_sequences=True))
+        ae.add(LSTM(hidden_size,
+                    input_shape=(1, X_train.shape[2]),
+                    kernel_initializer=tf.keras.initializers.GlorotNormal(seed=0),
+                    bias_initializer=tf.keras.initializers.Zeros(),
+                    return_sequences=True))
+        ae.add(LSTM(X_train.shape[2],
+                    kernel_initializer=tf.keras.initializers.GlorotNormal(seed=0),
+                    bias_initializer=tf.keras.initializers.Zeros(),
+                    return_sequences=True))
 
         opt_factory_ae = OptimizerFactory(opt=opt,
                                           lr_schedule=True,
@@ -178,112 +183,154 @@ def train_cf(params):
     global best_val_acc
     global SavedParameters
 
-    model_input = Input(shape=(1, XGlobal.shape[2]))
-    y = LSTM(units=params['unit1'], activation='tanh', return_sequences=True)(model_input)
-    y = Dropout(params['dropout'])(y)
-    y = LSTM(units=params['unit2'], activation='tanh')(y)
-    output = Dense(YGlobal.shape[1],
-                   activation='softmax')(y)
-    model = tf.keras.Model(inputs=model_input, outputs=output)
+    global result_path
+    global load_previous_result
+    global continue_loading
 
-    model.compile(optimizer=Adam(learning_rate=params['learning_rate']),
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy']
-                  )
+    if (result_path is not None) and continue_loading:
+        result_table = pd.read_csv(result_path)
 
-    model_checkpoint = tf.keras.callbacks.ModelCheckpoint(
-        filepath=os.path.join(CHECKPOINT_PATH_, 'best_model.h5'),
-        save_weights_only=True,
-        monitor='val_loss',
-        mode='auto',
-        save_best_only=True
-    )
-    early_stop = tf.keras.callbacks.EarlyStopping(
-        monitor="val_loss",
-        min_delta=0.0001,
-        patience=10,
-        mode="auto",
-        restore_best_weights=True
-    )
-    cf_start = time.time()
-    model.fit(XGlobal, YGlobal,
-              validation_data=(XValGlobal, YValGlobal),
-              epochs=50,
-              batch_size=params['batch_size'],
-              callbacks=[model_checkpoint, early_stop],
-              verbose=2)
-    cf_time = (time.time() - cf_start)
+        tid += 1
+        selected_row = result_table[round(result_table['learning_rate'], 5) == round(params['learning_rate'], 5)]
+        print(selected_row)
+        loss_hp = selected_row['F1_val'].values[0]
+        loss_hp = -loss_hp
+        if tid == len(result_table):
+            continue_loading = False
 
-    Y_predicted = model.predict(XValGlobal, workers=4, verbose=2)
+        if load_previous_result:
+            best_val_acc = result_table['F1_val'].max()
 
-    y_val = np.argmax(YValGlobal, axis=1)
-    Y_predicted = np.argmax(Y_predicted, axis=1)
+            result_table = result_table.sort_values('F1_val', ascending=False)
+            SavedParameters = result_table.to_dict(orient='records')
+            with open((os.path.join(SAVE_PATH_, 'best_result.csv')), 'w', newline='') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=SavedParameters[0].keys())
+                writer.writeheader()
+                writer.writerows(SavedParameters)
 
-    cf_val = metrics.confusion_matrix(y_val, Y_predicted)
-    acc_val = metrics.accuracy_score(y_val, Y_predicted)
-    precision_val = metrics.precision_score(y_val, Y_predicted, average='binary')
-    recall_val = metrics.recall_score(y_val, Y_predicted, average='binary')
-    f1_val = metrics.f1_score(y_val, Y_predicted, average='binary')
+            load_previous_result = False
 
-    test_start_time = time.time()
-    pred = model.predict(XTestGlobal, workers=4, verbose=2)
-    test_elapsed_time = time.time() - test_start_time
+    else:
+        tid += 1
+        tf.keras.backend.clear_session()
+        model_input = Input(shape=(1, XGlobal.shape[2]))
+        y = LSTM(units=params['unit1'],
+                 activation='tanh',
+                 kernel_initializer=tf.keras.initializers.GlorotNormal(seed=0),
+                 bias_initializer=tf.keras.initializers.Zeros(),
+                 return_sequences=True)(model_input)
+        y = Dropout(params['dropout'])(y)
+        y = LSTM(units=params['unit2'],
+                 activation='tanh',
+                 kernel_initializer=tf.keras.initializers.GlorotNormal(seed=0),
+                 bias_initializer=tf.keras.initializers.Zeros())(y)
+        output = Dense(YGlobal.shape[1],
+                       kernel_initializer=tf.keras.initializers.GlorotNormal(seed=0),
+                       bias_initializer=tf.keras.initializers.Zeros(),
+                       activation='softmax')(y)
+        model = tf.keras.Model(inputs=model_input, outputs=output)
 
-    pred = np.argmax(pred, axis=1)
-    y_eval = np.argmax(YTestGlobal, axis=1)
+        trainable_params = sum([tf.size(w).numpy() for w in model.trainable_variables])
+        model.compile(optimizer=Adam(learning_rate=params['learning_rate']),
+                      loss='categorical_crossentropy',
+                      metrics=['accuracy']
+                      )
 
-    cf_test = metrics.confusion_matrix(y_eval, pred)
-    acc_test = metrics.accuracy_score(y_eval, pred)
-    precision_test = metrics.precision_score(y_eval, pred, average='binary')
-    recall_test = metrics.recall_score(y_eval, pred, average='binary')
-    f1_test = metrics.f1_score(y_eval, pred, average='binary')
+        model_checkpoint = tf.keras.callbacks.ModelCheckpoint(
+            filepath=os.path.join(CHECKPOINT_PATH_, 'best_model.h5'),
+            save_weights_only=True,
+            monitor='val_loss',
+            mode='auto',
+            save_best_only=True
+        )
+        early_stop = tf.keras.callbacks.EarlyStopping(
+            monitor="val_loss",
+            min_delta=0.0001,
+            patience=10,
+            mode="auto",
+            restore_best_weights=True
+        )
+        cf_start = time.time()
+        model.fit(XGlobal, YGlobal,
+                  validation_data=(XValGlobal, YValGlobal),
+                  epochs=50,
+                  batch_size=params['batch_size'],
+                  callbacks=[model_checkpoint, early_stop],
+                  verbose=2)
+        cf_time = (time.time() - cf_start)
 
-    result = {
-        "unit1": params["unit1"],
-        "unit2": params["unit2"],
-        "learning_rate": params["learning_rate"],
-        "batch_size": params["batch_size"],
-        "dropout": params["dropout"],
-        "cf_time": cf_time,
-        "TP_val": cf_val[0][0],
-        "FP_val": cf_val[0][1],
-        "TN_val": cf_val[1][1],
-        "FN_val": cf_val[1][0],
-        "OA_val": acc_val,
-        "P_val": precision_val,
-        "R_val": recall_val,
-        "F1_val": f1_val,
-        "test_time": int(test_elapsed_time),
-        "TP_test": cf_test[0][0],
-        "FP_test": cf_test[0][1],
-        "FN_test": cf_test[1][0],
-        "TN_test": cf_test[1][1],
-        "OA_test": acc_test,
-        "P_test": precision_test,
-        "R_test": recall_test,
-        "F1_test": f1_test,
-    }
+        Y_predicted = model.predict(XValGlobal, workers=4, verbose=2)
 
-    SavedParameters.append(result)
+        y_val = np.argmax(YValGlobal, axis=1)
+        Y_predicted = np.argmax(Y_predicted, axis=1)
 
-    if SavedParameters[-1]["F1_val"] > best_val_acc:
-        print("new model saved:" + str(SavedParameters[-1]))
-        model.save(os.path.join(SAVE_PATH_, Name.replace(".csv", "_model.h5")))
-        del model
-        best_val_acc = SavedParameters[-1]["F1_val"]
+        cf_val = metrics.confusion_matrix(y_val, Y_predicted)
+        acc_val = metrics.accuracy_score(y_val, Y_predicted)
+        precision_val = metrics.precision_score(y_val, Y_predicted, average='binary')
+        recall_val = metrics.recall_score(y_val, Y_predicted, average='binary')
+        f1_val = metrics.f1_score(y_val, Y_predicted, average='binary')
 
-    SavedParameters = sorted(SavedParameters, key=lambda i: i['F1_val'], reverse=True)
+        test_start_time = time.time()
+        pred = model.predict(XTestGlobal, workers=4, verbose=2)
+        test_elapsed_time = time.time() - test_start_time
 
-    try:
-        with open((os.path.join(SAVE_PATH_, 'best_result.csv')), 'w', newline='') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=SavedParameters[0].keys())
-            writer.writeheader()
-            writer.writerows(SavedParameters)
-    except IOError:
-        print("I/O error")
+        pred = np.argmax(pred, axis=1)
+        y_eval = np.argmax(YTestGlobal, axis=1)
 
-    loss_hp = -result["F1_val"]
-    gc.collect()
+        cf_test = metrics.confusion_matrix(y_eval, pred)
+        acc_test = metrics.accuracy_score(y_eval, pred)
+        precision_test = metrics.precision_score(y_eval, pred, average='binary')
+        recall_test = metrics.recall_score(y_eval, pred, average='binary')
+        f1_test = metrics.f1_score(y_eval, pred, average='binary')
+
+        result = {
+            "tid": tid,
+            "n_params": trainable_params,
+            "unit1": params["unit1"],
+            "unit2": params["unit2"],
+            "learning_rate": params["learning_rate"],
+            "batch_size": params["batch_size"],
+            "dropout": params["dropout"],
+            "cf_time": cf_time,
+            "TP_val": cf_val[0][0],
+            "FP_val": cf_val[0][1],
+            "TN_val": cf_val[1][1],
+            "FN_val": cf_val[1][0],
+            "OA_val": acc_val,
+            "P_val": precision_val,
+            "R_val": recall_val,
+            "F1_val": f1_val,
+            "test_time": int(test_elapsed_time),
+            "TP_test": cf_test[0][0],
+            "FP_test": cf_test[0][1],
+            "FN_test": cf_test[1][0],
+            "TN_test": cf_test[1][1],
+            "OA_test": acc_test,
+            "P_test": precision_test,
+            "R_test": recall_test,
+            "F1_test": f1_test,
+        }
+
+        SavedParameters.append(result)
+
+        if SavedParameters[-1]["F1_val"] > best_val_acc:
+            print("new model saved:" + str(SavedParameters[-1]))
+            model.save(os.path.join(SAVE_PATH_, Name.replace(".csv", "_model.h5")))
+            del model
+            best_val_acc = SavedParameters[-1]["F1_val"]
+
+        SavedParameters = sorted(SavedParameters, key=lambda i: i['F1_val'], reverse=True)
+
+        try:
+            with open((os.path.join(SAVE_PATH_, 'best_result.csv')), 'w', newline='') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=SavedParameters[0].keys())
+                writer.writeheader()
+                writer.writerows(SavedParameters)
+        except IOError:
+            print("I/O error")
+
+        loss_hp = -result["F1_val"]
+        gc.collect()
     return {'loss': loss_hp, 'status': STATUS_OK}
 
 
@@ -338,8 +385,8 @@ def hyperparameter_tuning(dataset_name):
     XGlobal = np.reshape(XGlobal, (XGlobal.shape[0], 1, XGlobal.shape[1])).astype('float32')
     XTestGlobal = np.reshape(XTestGlobal, (XTestGlobal.shape[0], 1, XTestGlobal.shape[1])).astype('float32')
 
-    YGlobal = keras.utils.to_categorical(YGlobal, num_classes=2)
-    YTestGlobal = keras.utils.to_categorical(YTestGlobal, num_classes=2)
+    YGlobal = tf.keras.utils.to_categorical(YGlobal, num_classes=2)
+    YTestGlobal = tf.keras.utils.to_categorical(YTestGlobal, num_classes=2)
 
     XGlobal, XValGlobal, YGlobal, YValGlobal = train_test_split(XGlobal,
                                                                 YGlobal,
@@ -353,8 +400,10 @@ def hyperparameter_tuning(dataset_name):
     print('test set:', XTestGlobal.shape, YTestGlobal.shape)
 
     ae_hyperparameters_to_optimize = {
-        "hidden_size": [32, 64, 128],
-        "optimizer": ['sgd', 'adam']
+        # "hidden_size": [32, 64, 128],
+        # "optimizer": ['sgd', 'adam']
+        "hidden_size": [128],
+        "optimizer": ['sgd']
     }
     keys = list(ae_hyperparameters_to_optimize.keys())
     values = list(ae_hyperparameters_to_optimize.values())
@@ -390,6 +439,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Description of your script')
     parser.add_argument('--dataset', type=str, default='UNSW_NB15', required=True,
                         help='dataset name choose from: "UNSW", "KDD", "CICIDS"')
+    parser.add_argument('--result', type=str, required=False,
+                        help='path of hyper-parameter training result table .csv file')
 
     args = parser.parse_args()
+
+    if args.result is not None:
+        result_path = args.result
+    else:
+        result_path = None
+
     hyperparameter_tuning(args.dataset)
